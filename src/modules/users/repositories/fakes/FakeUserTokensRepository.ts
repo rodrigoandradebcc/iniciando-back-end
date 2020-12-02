cbd882cf130f2314b33229cc5680db12a3a8bc32
Rod Andrade <rodrigoandradebcc@gmail.com>
2020-12-02 07:30:15 -0300

    Reset de senha

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -16,6 +16,15 @@ class FakeUserTokensRepository implements IUserTokensReposiotory {
         });
 
         this.userTokens.push(userToken);
+
+        return userToken;
+    }
+
+    public async findByToken(token: string): Promise<UserToken | undefined> {
+        const userToken = this.userTokens.find(
+            findToken => findToken.token === token,
+        );
+
         return userToken;
     }
 }
