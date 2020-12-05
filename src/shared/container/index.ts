6ceb4545cc8829c49b525bbb103a638263451037
Rod Andrade <rodrigoandradebcc@gmail.com>
2020-12-05 00:57:32 -0300

    Salvando tokens no banco

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,6 +9,9 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UsersTokensRepository';
+
 // import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 // import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
@@ -20,4 +23,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
     'UsersRepository',
     UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+    'UsersRepository',
+    UserTokensRepository,
 );
--- src/shared/infra/typeorm/migrations/1599076321060-AlterProviderFieldToProviderId.ts
+++ src/shared/infra/typeorm/migrations/1599076321060-AlterProviderFieldToProviderId.ts
@@ -1,37 +1,51 @@
-import {MigrationInterface, QueryRunner, TableColumn, TableForeignKey} from "typeorm";
+import {
+    MigrationInterface,
+    QueryRunner,
+    TableColumn,
+    TableForeignKey,
+} from 'typeorm';
 
-export default class AlterProviderFieldToProviderId1599076321060 implements MigrationInterface {
-
+export default class AlterProviderFieldToProviderId1599076321060
+    implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropColumn('appointments','provider');
+        await queryRunner.dropColumn('appointments', 'provider');
 
-        await queryRunner.addColumn('appointments', new TableColumn({
-            name: 'provider_id',
-            type: 'uuid',
-            isNullable: true,
+        await queryRunner.addColumn(
+            'appointments',
+            new TableColumn({
+                name: 'provider_id',
+                type: 'uuid',
+                isNullable: true,
             }),
         );
 
-        await queryRunner.createForeignKey('appointments', new TableForeignKey({
-            name: 'AppointmentsProvider',
-            columnNames: ['provider_id'],
-            referencedColumnNames: ['id'],
-            referencedTableName: 'users',
-            onDelete: 'SET NULL',
-            onUpdate: 'CASCADE',
-        }));
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropForeignKey('appointments','AppointmentsProvider');
-
-        await queryRunner.dropColumn('appointments','provider_id');
-
-        await queryRunner.addColumn('appointments', new TableColumn({
-            name: 'provider',
-            type: 'varchar',
-        })
+        await queryRunner.createForeignKey(
+            'appointments',
+            new TableForeignKey({
+                name: 'AppointmentsProvider',
+                columnNames: ['provider_id'],
+                referencedColumnNames: ['id'],
+                referencedTableName: 'users',
+                onDelete: 'SET NULL',
+                onUpdate: 'CASCADE',
+            }),
         );
     }
 
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey(
+            'appointments',
+            'AppointmentsProvider',
+        );
+
+        await queryRunner.dropColumn('appointments', 'provider_id');
+
+        await queryRunner.addColumn(
+            'appointments',
+            new TableColumn({
+                name: 'provider',
+                type: 'varchar',
+            }),
+        );
+    }
 }
